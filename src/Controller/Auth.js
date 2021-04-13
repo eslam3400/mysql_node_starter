32008c028f0b1f2167f742dd996e664cf8361969
Eslam Magdy <eslam3400@gmail.com>
2021-04-13 17:11:48 +0200

    all is good now for v1.0

--- src/Controller/Auth.js
+++ src/Controller/Auth.js
@@ -5,15 +5,14 @@ let loginPage = (req, res) => {
 }
 
 let login = (req, res) => {
-  new Model('users').getOne({ feild: "email", operator: "==", value: req.body.email }, (userId, user) => {
-    if (req.body.password == user.password)
-      res.cookie('token', userId, { maxAge: 43200000 }).redirect('/')
+  new Model('users').get({ where: `email = '${req.body.email}' AND password = '${req.body.password}'` }, data => {
+    if (data.length > 0) res.cookie('token', data[0].id, { maxAge: 43200000 }).redirect('/')
   })
 }
 
 let signup = (req, res) => {
   let user = req.body
-  new Model('users').add({ user })
+  new Model('users').add(user)
 }
 
 let logout = (req, res) => {
--- src/Middleware/Authorization.js
+++ src/Middleware/Authorization.js
@@ -2,8 +2,8 @@ const Model = require('../Model/Model')
 
 let auth = (req, res, next) => {
   if (req.cookies.token == undefined || req.cookies.token == null) res.redirect('/')
-  else new Model('users').getOneByDocId(req.cookies.token, data => {
-    if (data.role == 'admin') next()
+  else new Model('users').get({ where: `id = ${req.cookies.token}` }, data => {
+    if (data.length > 0 && data[0].role == 'admin') next()
     else res.redirect('/')
   })
 }
