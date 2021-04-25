6cf88c2625289bbfdc7949256058cff5672d4ebe
Eslam Magdy <eslam3400@gmail.com>
2021-04-25 07:02:07 +0200

    fix bug :"D

--- src/Controller/Auth.js
+++ src/Controller/Auth.js
@@ -4,10 +4,10 @@ let loginPage = (req, res) =>
   res.render('login', { message: "" })
 
 let login = (req, res) =>
-  new Model('users').get({ where: `email = '${req.body.email}' AND password = '${req.body.password}'` }, data => {
+  new Model('users').get(data => {
     if (data.length > 0) res.cookie('token', data[0].id, { maxAge: 43200000 }).redirect('/')
     else res.render('/login', { message: "loginError" })
-  })
+  }, { where: `email = '${req.body.email}' AND password = '${req.body.password}'` })
 
 let signupPage = (req, res) =>
   res.render('signup')
--- src/Middleware/Authorization.js
+++ src/Middleware/Authorization.js
@@ -1,10 +1,10 @@
 const Model = require('../Model/Model')
 
 let admin = (req, res, next) => {
-  new Model('users').get({ where: `id = ${req.cookies.token}` }, data => {
+  new Model('users').get(data => {
     if (data.length > 0 && data[0].role == 'admin') next()
     else res.render('/login', { message: "accessError" })
-  })
+  }, { where: `id = ${req.cookies.token}` })
 }
 
 module.exports = { admin }
