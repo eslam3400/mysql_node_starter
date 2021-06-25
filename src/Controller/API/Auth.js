e2fd21b550d810ceee1185e59bb92cb368402d97
Eslam Magdy <eslam3400@gmail.com>
2021-06-25 11:20:11 +0200

    fix auth controller and middleware bug and tested for the api

--- src/Controller/API/Auth.js
+++ src/Controller/API/Auth.js
@@ -2,19 +2,19 @@ const db = require('../../Model/Model')
 const { createToken, verifyToken } = require('../../Utilities/Utility')
 
 let login = async (req, res) => {
-  let users = await db.target(`users`).get({ where: `email = '${req.body.email}' AND password = '${req.body.password}'` })
+  let users = await db.target(`users`).get({ where: `username = '${req.body.username}' AND password = '${req.body.password}'` })
   if (users.length == 1) res.status(200).json({ token: createToken(users[0].id) })
   else res.status(404).json({ err: "user not founded" })
 }
 
 let signup = async (req, res) => {
-  let result = await db.add(req.body, { unique: [{ key: `email`, value: `${req.body.email}` }] })
-  if (result == null) res.status(406).json({ err: `error creating new user :(` })
+  let result = await db.target('users').add(req.body, ['username'])
+  if (result == null) res.status(406).json({ err: `error creating new user cause this username is already in use` })
   else res.status(201).json({ msg: `user created successfully :D` })
 }
 
 let userData = async (req, res) => {
-  let users = await db.get({ where: `id = '${verifyToken(req.get('Token'))}'` })
+  let users = await db.target('users').get({ where: `id = '${verifyToken(req.get('Token'))}'` })
   if (users.length == 1) res.status(200).json(users[0])
   else res.status(404).json({ err: `user not founded` })
 }
--- src/Middleware/API/Authorization.js
+++ src/Middleware/API/Authorization.js
@@ -1,17 +1,17 @@
-const UserModel = require('../../Model/Model')
+const db = require('../../Model/Model')
 const { verifyToken } = require('../../Utilities/Utility')
 
 let auth = async (req, res, next) => {
   if (req.get('Token') == undefined || req.get('Token') == null) return res.status(404).json({ msg: 'Not Authourized' })
   else {
-    let users = await new UserModel().get({ where: `id = '${verifyToken(req.get('Token'))}'` })
+    let users = await db.target('users').get({ where: `id = '${verifyToken(req.get('Token'))}'` })
     if (users.length == 1) return next()
     else return res.json({ msg: "Not Authenticated" })
   }
 }
 
 let admin = async (req, res, next) => {
-  let users = await new UserModel().get({ where: `id = '${verifyToken(req.get('Token'))}'` })
+  let users = await db.target("users").get({ where: `id = '${verifyToken(req.get('Token'))}'` })
   if (users.length == 1 && users[0].role == 'admin') return next()
   else return res.json({ msg: "Not Authorized" })
 }
